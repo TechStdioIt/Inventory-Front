7c92d8396f429fccd0fd805d84afc1db2729d85a
SNP Sujon <18203037@iubat.edu>
2025-04-18 19:13:44 +0600

    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,12 @@ export const environment = {
   //Development Environment on 26-11-2024
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'https://ims.techstdio.com/api/',
+  apiUrl: 'https://localhost:7136/api/',
   // apiUrl: 'https://localhost:7136/api/',
   reportUrl:'https://localhost:7136/',
   uploadFileUrl:'D:/project/Inventory-Front/src/assets/images/uploadedImages'
+
+  
 
 
 
