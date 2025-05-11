5d35cd9915c0b5ec2f444a5533362d7f561d3123
SNP Sujon <18203037@iubat.edu>
2025-05-11 16:11:55 +0600

    Update AuthInterceptor.ts

--- src/app/Authorization/AuthInterceptor.ts
+++ src/app/Authorization/AuthInterceptor.ts
@@ -5,17 +5,29 @@ import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private toastr:ToastrService,private router:Router){}
+ constructor(private toastr: ToastrService, private router: Router) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const token = localStorage.getItem('token');
+   var excludedUrls: string[] = [
+    '/Administrator/Login',
+    '/Branch/GetBranchListByMaster',
+    'Administrator/GetDropdownData'
+  ];
+
+    // Skip interceptor logic for login API
+  const isExcluded = excludedUrls.some(url => req.url.includes(url));
+    if (isExcluded) {
+      return next.handle(req); // Skip token validation
+    }
 
     if (token) {
       const cloned = req.clone({
         setHeaders: { Authorization: `Bearer ${token}` }
       });
       return next.handle(cloned);
-    }else{
-      this.toastr.error('Your Token has been expired','Again Login');
+    } else {
+      this.toastr.error('Your Token has expired', 'Please log in again');
       this.router.navigate(['/']);
     }
 
