fd69b59b7e825ce88c059015d5dbfe9dab9d58dd
Munshi Faysal Ahmed <18203036@iubat.edu>
2025-05-12 12:58:08 +0600

    Update HttpClientConnection.service.ts

--- src/app/Services/HttpClientConnection.service.ts
+++ src/app/Services/HttpClientConnection.service.ts
@@ -12,62 +12,97 @@ export class HttpClientConnectionService {
 
   constructor(
     private http: HttpClient
-    )
-   { 
-    
-   }
+  ) {
 
-    GetData(_url: string) {
-      _url = createUrl(_url) 
-      return this.http.get(_url, { withCredentials: true }).pipe(first());
-    }
-    GetDataById(_url: string, _id: string) {
-        _url = createUrl(_url)+ "/" + _id;
-        return this.http.get(_url, { withCredentials: true }).pipe(catchError(this.handleError));
-    }
-    GetDataByParams(_url: string, _params: any) {
-        _url = createUrl(_url);
-        return this.http.get(_url, { params: _params, withCredentials: true }).pipe(catchError(this.handleError));
-    }
-    PostData(_url: string, _postdata: any) {
-        _url = createUrl(_url)
-        return this.http.post(_url, _postdata, { withCredentials: true }).pipe(catchError(this.handleError));
-    } 
-    PostDataWithFile(_url: string, _postdata: any) {
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Accept': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        })
-      };
-      _url = createUrl(_url)
-      return this.http.post(_url, _postdata, httpOptions).pipe(catchError(this.handleError));
+  }
 
-    }
+  GetData(_url: string) {
+    _url = createUrl(_url);
 
-    PutData(_url: string,_params:any, _putdata: any) {
-        _url = createUrl(_url)+"/"+_params;
-        return this.http.put(_url, _putdata, { withCredentials: true }).pipe(catchError(this.handleError));
-    }
+    const headers = new HttpHeaders({
+      'X-Client-Url': window.location.href // or any custom header
+    });
 
-    PutDataWithFile(_url: string, _putdata: any,_params:any){
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Accept': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        })
-      };
-      _url = createUrl(_url)+"/"+_params
-      return this.http.put(_url, _putdata, httpOptions).pipe(catchError(this.handleError));
+    return this.http.get(_url, {
+      headers,
+      withCredentials: true
+    }).pipe(first());
+  }
 
-    }
+ GetDataById(_url: string, _id: string) {
+  _url = createUrl(_url) + "/" + _id;
+  return this.http.get(_url, {
+    headers: this.getClientUrlHeaders(),
+    withCredentials: true
+  }).pipe(catchError(this.handleError));
+}
 
-    DeleteData(_url: string) {
-        _url = createUrl(_url);
-        return this.http.delete(_url, { withCredentials: true }).pipe(catchError(this.handleError));
-    }
-    private handleError(error: Response) {
-      return throwError(error);
-    } 
+
+ GetDataByParams(_url: string, _params: any) {
+  _url = createUrl(_url);
+  return this.http.get(_url, {
+    params: _params,
+    headers: this.getClientUrlHeaders(),
+    withCredentials: true
+  }).pipe(catchError(this.handleError));
+}
+
+PostData(_url: string, _postdata: any) {
+  _url = createUrl(_url);
+  return this.http.post(_url, _postdata, {
+    headers: this.getClientUrlHeaders(),
+    withCredentials: true
+  }).pipe(catchError(this.handleError));
+}
+
+  PostDataWithFile(_url: string, _postdata: any) {
+  const headers = this.getClientUrlHeaders().set('Accept', 'application/json');
+
+  _url = createUrl(_url);
+  return this.http.post(_url, _postdata, {
+    headers,
+    withCredentials: true
+  }).pipe(catchError(this.handleError));
+}
+
+
+  PutData(_url: string, _params: any, _putdata: any) {
+  _url = createUrl(_url) + "/" + _params;
+  return this.http.put(_url, _putdata, {
+    headers: this.getClientUrlHeaders(),
+    withCredentials: true
+  }).pipe(catchError(this.handleError));
+}
+
+
+  PutDataWithFile(_url: string, _putdata: any, _params: any) {
+  const headers = this.getClientUrlHeaders().set('Accept', 'application/json');
+
+  _url = createUrl(_url) + "/" + _params;
+  return this.http.put(_url, _putdata, {
+    headers
+  }).pipe(catchError(this.handleError));
+}
+
+
+  DeleteData(_url: string) {
+  _url = createUrl(_url);
+  return this.http.delete(_url, {
+    headers: this.getClientUrlHeaders(),
+    withCredentials: true
+  }).pipe(catchError(this.handleError));
+}
+
+  private handleError(error: Response) {
+    return throwError(error);
+  }
+
+
+  private getClientUrlHeaders(): HttpHeaders {
+  return new HttpHeaders({
+    'X-Client-Url': window.location.href
+  });
+}
+
 
 }
